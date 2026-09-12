51e5a787257f70b4b1127ef9f16b26dd27455d5e
Weslley F. Souza <weslley.fernandes@aluno.ufca.edu.br>
2026-09-12 17:29:25 -0300

    build: adiciona esqueleto componente wifi

---
+++ components/wifi/include/wifi.h
@@ -0,0 +1,16 @@
+#ifndef COMPONENT_WIFI_H
+#define COMPONENT_WIFI_H
+
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t wifi_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMPONENT_WIFI_H
---
+++ components/wifi/wifi.c
@@ -0,0 +1,11 @@
+#include "wifi.h"
+
+#include <esp_log.h>
+
+const char *TAG = "WIFI";
+
+esp_err_t wifi_init() {
+	esp_err_t err;
+
+	return ESP_OK;
+}
